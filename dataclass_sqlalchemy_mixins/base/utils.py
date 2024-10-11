f167edd5d5ebfd20d1220be8d405334cce4d3b52
Viachelsav Koniukhov <51295486+ViAchKoN@users.noreply.github.com>
2024-10-11 12:37:43 -0600

    feature: added utils to allow apply filters and orderins without using dataclasses (#27)

--- dataclass_sqlalchemy_mixins/base/utils.py
+++ dataclass_sqlalchemy_mixins/base/utils.py
@@ -24,3 +24,61 @@ def get_unary_expressions(
     return SqlAlchemyOrderConverterMixin().get_unary_expressions(
         order_by=order_by, model=model
     )
+
+
+def apply_filters(
+    query,
+    filters: tp.Dict[str, tp.Any],
+    model: tp.Type[DeclarativeMeta] = None,
+):
+    converter = SqlAlchemyFilterConverterMixin()
+
+    filters_binary_expressions = converter.get_models_binary_expressions(
+        model=model,
+        filters=filters,
+    )
+
+    models_to_join = []
+    binary_expressions = []
+
+    for binary_expression in filters_binary_expressions:
+        models_to_join += binary_expression["models"]
+        binary_expressions.append(binary_expression["binary_expression"])
+
+    # Checking if there are other models required to be joined
+    if models_to_join != [
+        model,
+    ]:
+        query = converter.join_models(query=query, models=models_to_join)
+
+    query = query.filter(*binary_expressions)
+    return query
+
+
+def apply_order_by(
+    query,
+    order_by: tp.Union[str, tp.List[str]],
+    model: tp.Type[DeclarativeMeta] = None,
+):
+    converter = SqlAlchemyOrderConverterMixin()
+
+    order_by_unary_expressions = converter.get_models_unary_expressions(
+        model=model,
+        order_by=order_by,
+    )
+
+    models_to_join = []
+    unary_expressions = []
+
+    for binary_expression in order_by_unary_expressions:
+        models_to_join += binary_expression["models"]
+        unary_expressions.append(binary_expression["unary_expression"])
+
+    # Checking if there are other models required to be joined
+    if models_to_join != [
+        model,
+    ]:
+        query = converter.join_models(query=query, models=models_to_join)
+
+    query = query.order_by(*unary_expressions)
+    return query
